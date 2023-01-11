613795cc20a0750eb98d94cfe355ce726c2b1e5a
Pandylom <107990275+Pandylom@users.noreply.github.com>
2023-01-11 18:27:02 +0200

    Update settings.py

--- ambax/settings.py
+++ ambax/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ts#sv^@7z(swz(b2aw88twm&f=l^7k5e63_guqzxmttk5d3fua'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
